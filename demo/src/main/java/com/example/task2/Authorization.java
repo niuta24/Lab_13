5715c98a601d39fe2b0dffd3cffac72dc9af6f66
niuta24 <syvkova.pn@ucu.edu.ua>
2023-12-13 12:47:43 +0300

    changes

--- demo/src/main/java/com/example/task2/Authorization.java
+++
@@ -1,7 +0,0 @@
-package com.example.task2;
-
-public class Authorization extends Авторизація {
-    public boolean login(DataBase db) {
-        return авторизуватися(db);
-    }
-}
---
+++ src/main/java/com/example/task1/User.java
@@ -0,0 +1,12 @@
+package com.example.task1;
+
+import java.time.LocalDate;
+
+public interface User {
+    public String getEmail();
+
+    public String getCountry();
+
+    public LocalDate getActiveTime();
+
+}
